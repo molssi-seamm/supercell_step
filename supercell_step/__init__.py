cc0788e77da1ea71ea29d7730f5eb1ac534a7e7a
Paul Saxe <paul@thesaxes.net>
2021-03-15 02:33:56 -0400

    Fixed yapf error.

--- supercell_step/__init__.py
+++ supercell_step/__init__.py
@@ -15,6 +15,7 @@ from supercell_step.tk_supercell import TkSupercell  # noqa: F401, E501
 
 # Handle versioneer
 from ._version import get_versions
+
 __author__ = """Paul Saxe"""
 __email__ = 'psaxe@molssi.org'
 versions = get_versions()
